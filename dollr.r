fc02014bedf7651a87a07ceb79af10db162217cc
Jesus Perez <japbogado@gmail.com>
2019-01-13 16:16:41 -0400

    DRY the script

--- dollr.r
+++ dollr.r
@@ -12,42 +12,53 @@ if (length(cmdargs) == 0) {
     cmdargs <- "airtm"
 }
 
+init <- function(msg) write(sprintf("Fetching %s\n", msg), stdout())
+
 if (cmdargs[1] == "twitter") {
-    # @MonitorDolarVe
-    write("Retreiving @MonitorDolarVe tweets.", stdout())
+    init("@MonitorDolarVe tweets.")
+
     tw <- rtweet::get_timeline("MonitorDolarVe")
     tw <- tw[grep("^Dólar paralelo", tw$text),][1,]
-    rate <- gsub(
+
+    rt <- gsub(
         "^.*(Promedio general = .+BsS).*$",
         "\\1",
         tw[["text"]]
     )
+    write_rate <- function(rate = rt) write(sprintf("%s", rate), stdout()) 
+
     when <- as.POSIXlt(tw[["created_at"]], tz = Sys.timezone())
     when <- strftime(when, format = "%H:%M %d/%m/%Y")
-    write(sprintf("USD/VES rates @ [%s]", when), stdout())
-    write(sprintf("%s", rate), stdout())
 
 } else if (cmdargs[1] == "airtm") {
-    # AirTM
-    write("Retreiving airtmrates' rates.", stdout())
-    rates <- read.csv("https://airtmrates.com/rates")
-    rates <- rates[grep("VES", rates$Code), c("Method", "Rate", "Buy", "Sell")]
+    init("airtmrates' rates.")
+
+    rt <- read.csv("https://airtmrates.com/rates")
+    rt <- rt[grep("VES", rt$Code), c("Method", "Rate", "Buy", "Sell")]
+    write_rate <- function(rate = rt) write(knitr::kable(rate, row.names = FALSE), stdout()) 
+
     when <- strftime(Sys.time(), format = "%H:%M %d/%m/%Y")
-    write(sprintf("USD/VES rates @ [%s]", when), stdout())
-    write(knitr::kable(rates, row.names = FALSE), stdout())
 
 } else {
     # Help Message
+    if (cmdargs[1] != "help") write(sprintf("Unrecognized argument \"%s\"\n", cmdargs[1]), stdout())
+    cmdargs <- "help"
     help_msg <- paste(
         "Retreive from the internet the current USD/VES price",
         "Usage: dollar [SOURCE]",
         "Example: dollar airtm\n",
         "The current supported SOURCE values are:",
-        "\tairtm\tGets the price from airtmrates.com",
-        "\ttwitter\tGets the price from the last @MonitorDolarVe tweet. (The default)",
+        "\tairtm\tGets the price from airtmrates.com (The default)",
+        "\ttwitter\tGets the price from the last @MonitorDolarVe tweet.",
         "\tAny other value will return this help message",
         sep = "\n"
     )
     write(help_msg, stdout())
+
+}
+
+if (cmdargs != "help") {
+    write(sprintf("USD/VES rates @ [%s]\n", when), stdout())
+    write_rate()
 
 }
