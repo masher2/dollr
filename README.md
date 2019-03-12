66457d1a6b3d09cd0b3c6628414f4c29fbc41c54
Jesus Perez <japbogado@gmail.com>
2019-03-12 08:19:57 -0400

    Remove twitter support

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Inspired in the sudden skyrocketting (+300% in 7 days (03/01/2019 to 10/01/2019)) of the USD price related to the VES and the burden that represents looking for the rate manually I developed this R script to save some minutes of my time every week.
 
-This script only depends (as of now) in [rtweet](https://rtweet.info/) and [knitr](https://yihui.name/knitr/), I intend to keep the dependencies to a minimum while maybe adding more sources in the future.
+This script only depends (as of now) in [knitr](https://yihui.name/knitr/), I intend to keep the dependencies to a minimum while maybe adding more sources in the future.
 
 ## Usage
 
@@ -10,10 +10,9 @@ Add this script to yout `PATH` and call it from the terminal, the optional argum
 
 Example:
 
-` dollr.r twitter `
+` dollr.r`
 
 The current supported sources of information are:
 
-* `twitter`: Retrieves the rate from the last @MonitorDolarVe tweet. This may require your permission to use the twitter api.
 * `airtm`: Gets all the USD/VES rates from [airtmrates.io](https://airtmrates.com/). The default, requires `knitr` to dislay the results nicely on the terminal.
 
--- dollr.r
+++ dollr.r
@@ -1,9 +1,7 @@
 #! /usr/bin/Rscript
-# Get the parallel USD/VES rate from airtmrates.com or @MonitorDolarVe
-# Defaults to AirTM's rate because is faster
+# Get the parallel USD/VES rate from airtmrates.com
 #
 # This script depends on the following packages:
-#   rtweet
 #   knitr
 
 cmdargs <- commandArgs(trailingOnly=TRUE)
@@ -14,34 +12,22 @@ if (length(cmdargs) == 0) {
 
 init <- function(msg) write(sprintf("Fetching %s\n", msg), stdout())
 
-if (cmdargs[1] == "twitter") {
-    init("@MonitorDolarVe tweets.")
-
-    tw <- rtweet::get_timeline("MonitorDolarVe")
-    tw <- tw[grep("^Dólar paralelo", tw$text),][1,]
-
-    rt <- gsub(
-        "^.*(Promedio general = .+BsS).*$",
-        "\\1",
-        tw[["text"]]
-    )
-    write_rate <- function(rate = rt) write(sprintf("%s", rate), stdout()) 
-
-    when <- as.POSIXlt(tw[["created_at"]], tz = Sys.timezone())
-    when <- strftime(when, format = "%H:%M %d/%m/%Y")
-
-} else if (cmdargs[1] == "airtm") {
+if (cmdargs[1] == "airtm") {
     init("airtmrates' rates.")
 
     rt <- read.csv("https://airtmrates.com/rates")
     rt <- rt[grep("VES", rt$Code), c("Method", "Rate", "Buy", "Sell")]
-    write_rate <- function(rate = rt) write(knitr::kable(rate, row.names = FALSE), stdout()) 
+    write_rate <- function(rate = rt) {
+        write(knitr::kable(rate, row.names = FALSE), stdout()) 
+    }
 
     when <- strftime(Sys.time(), format = "%H:%M %d/%m/%Y")
 
 } else {
     # Help Message
-    if (cmdargs[1] != "help") write(sprintf("Unrecognized argument \"%s\"\n", cmdargs[1]), stdout())
+    if (cmdargs[1] != "help") {
+        write(sprintf("Unrecognized argument \"%s\"\n", cmdargs[1]), stdout())
+    }
     cmdargs <- "help"
     help_msg <- paste(
         "Retreive from the internet the current USD/VES price",
